1bb950ed9a3bf79daa7f8aaf6144b65f72555c37
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-06 13:25:09 +0500

    Create API for log in Users

--- api/login.php
+++ api/login.php
@@ -1,1 +1,10 @@
 <?php
+
+// Заголовки
+header("Access-Control-Allow-Origin: http://authentication-jwt/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Здесь будет соединение с БД
