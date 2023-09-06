a3ecf47927ef167d64e0d05e5a1738d2a8e8d810
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-06 13:12:54 +0500

    Create connct to db and setting

--- api/Config/Database.php
+++ api/Config/Database.php
@@ -1,1 +1,26 @@
 <?php
+
+// Используем для подключения к базе данных MySQL
+class Database
+{
+    // Учётные данные базы данных
+    private $host = "127.0.0.1";
+    private $db_name = "jwt-auth-api";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    // Получаем соединение с базой данных
+    public function getConnection()
+    {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        } catch (PDOException $exception) {
+            echo "Ошибка соединения с БД: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
--- api/create_user.php
+++ api/create_user.php
@@ -1,1 +1,22 @@
 <?php
+
+// Заголовки
+header("Access-Control-Allow-Origin: http://authentication-jwt/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Подключение к БД
+// Файлы, необходимые для подключения к базе данных
+include_once "./Config/Database.php";
+include_once "../Objects/User.php";
+
+// Получаем соединение с базой данных
+$database = new Database();
+$db = $database->getConnection();
+
+// Создание объекта "User"
+$user = new User($db);
+
+// Отправляемые данные будут здесь
